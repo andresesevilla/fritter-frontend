5416665b26704776f8ddc5c95ed29632a9bddbd8
Andres Sevilla <andresesevilla@gmail.com>
2022-11-01 01:49:03 -0400

    Added a lot of the logic needed to handle profile pages

---
+++ client/components/Profile/ProfileFreetsComponent.vue
@@ -0,0 +1,84 @@
+<!-- Display freets on profile -->
+
+<template>
+  <main>
+    <section v-if="isValidUsername">
+      <header>
+        <h2>Profile: @{{ $route.params.username }}</h2>
+      </header>
+      <!-- <section v-if="$store.state.freets.length">
+        <FreetComponent v-for="freet in $store.state.freets" :key="freet.id" :freet="freet" />
+      </section>
+      <article v-else>
+        <h3>No freets found.</h3>
+      </article> -->
+    </section>
+    <section v-else>
+      This is not a valid username (hopefully this can be replaced with the 404 page (without redirecting) in the future)
+    </section>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'ProfileFreets',
+  // components: { FreetComponent },
+  // async mounted() {
+  //   await this.getFreets();
+  // },
+  data() {
+    return { 
+      isValidUsername: true
+     };
+  },
+  // methods: {
+  //   async getFreets() {
+  //     const value = this.$route.params.username
+  //     const url = `/api/freets?author=${value}`;
+  //     try {
+  //       const r = await fetch(url);
+  //       const res = await r.json();
+  //       if (!r.ok) {
+  //         throw new Error(res.error);
+  //       }
+  //       this.$store.commit('updateFilter', value);
+  //       this.$store.commit('updateFreets', res);
+  //     } catch (e) {
+  //       if (value === this.$store.state.filter) {
+  //         this.$store.commit('updateFilter', null);
+  //         this.$store.commit('refreshFreets');
+  //       } else {
+  //         this.isValidUsername = false;
+  //       }
+  //       this.$set(this.alerts, e, 'error');
+  //       setTimeout(() => this.$delete(this.alerts, e), 3000);
+  //     }
+  //   }
+  // }
+};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+}
+
+header,
+header>* {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+button {
+  margin-right: 10px;
+}
+
+section .scrollbox {
+  flex: 1 0 50vh;
+  padding: 3%;
+  overflow-y: scroll;
+}
+</style>
--- client/components/Profile/ProfilePage.vue
+++ client/components/Profile/ProfilePage.vue
@@ -2,17 +2,19 @@
 
 <template>
   <div>
-    <p>You are viewing {{$route.params.username}}'s profile</p>
+    <ProfileFreetsComponent />
+    <!-- <p>You are viewing {{$route.params.username}}'s profile</p>
     <p>In the future, this will show their freets</p>
-    <p>If the username cannot be found, a 404 error will be shown</p>
+    <p>If the username cannot be found, a 404 error will be shown</p> -->
   </div>
 </template>
 
 <script>
+import ProfileFreetsComponent from '@/components/Profile/ProfileFreetsComponent.vue';
 
 export default {
   name: 'ProfilePage',
-  components: { },
+  components: { ProfileFreetsComponent },
   mounted() {
   }
 };
