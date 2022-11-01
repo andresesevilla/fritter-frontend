aaa28190b11c3a87d16c6e9878fbe2ef9a336448
Andres Sevilla <andresesevilla@gmail.com>
2022-10-31 23:55:27 -0400

    Combined homepage and login page

---
+++ client/components/Home/HomePage.vue
@@ -0,0 +1,24 @@
+<!-- Default page: login and feed -->
+
+<template>
+  <div>
+    <FreetsPage v-if="$store.state.username" />
+    <LoginPage v-else />
+  </div>
+</template>
+
+<script>
+import LoginPage from '@/components/Login/LoginPage.vue';
+import FreetsPage from '@/components/Freet/FreetsPage.vue';
+
+export default {
+  name: 'HomePage',
+  components: { LoginPage, FreetsPage },
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- client/router.ts
+++ client/router.ts
@@ -1,16 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import FreetsPage from './components/Freet/FreetsPage.vue';
+import HomePage from './components/Home/HomePage.vue';
 import AccountPage from './components/Account/AccountPage.vue';
-import LoginPage from './components/Login/LoginPage.vue';
 import NotFound from './NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', name: 'Home', component: FreetsPage },
+  { path: '/', name: 'Home', component: HomePage },
   { path: '/account', name: 'Account', component: AccountPage },
-  { path: '/login', name: 'Login', component: LoginPage },
   { path: '*', name: 'Not Found', component: NotFound }
 ];
 
@@ -19,15 +17,12 @@ const router = new VueRouter({ routes });
 /**
  * Navigation guards to prevent user from accessing wrong pages.
  */
-router.beforeResolve((to, from, next) => {
-  if (to.name === 'Login' && router.app.$store && router.app.$store.state.username) {
-    next({ name: 'Account' }); // Go to Account page if user navigates to Login and are signed in
-    return;
-  }
-
-  if (to.name !== 'Login' && (router.app.$store && !router.app.$store.state.username || router.app.$store == undefined)) {
-    next({ name: 'Login' }); // Go to Login page if user navigates to any other page and are not signed in
-    return;
+router.beforeEach((to, from, next) => {
+  if (router.app.$store) {
+    if (to.name !== 'Home' && !router.app.$store.state.username) {
+      next({ name: 'Home' }); // Go to Home page if user navigates to any other page and are not signed in
+      return;
+    }
   }
 
   next();
