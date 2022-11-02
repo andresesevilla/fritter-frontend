857b530e689b60299a1b37b27f0e1901ad485314
Andres Sevilla <andresesevilla@gmail.com>
2022-11-02 01:07:39 -0400

    Reworked the create freet form

--- client/components/Freet/CreateFreetForm.vue
+++ client/components/Freet/CreateFreetForm.vue
@@ -1,28 +1,113 @@
-<!-- Form for creating freets (block style) -->
+<!-- Form for creating freets (based on BlockForm) -->
+
+<template>
+  <form @submit.prevent="submit">
+    <h3>Create a freet</h3>
+    <article>
+      <div>
+        <label for="content">Content:</label>
+        <textarea name="content" v-model="content" id="content"></textarea>
+      </div>
+      <div v-if="$store.state.privatecircles.length">
+        <label for="privatecircle">Private Circle (Optional):</label>
+        <input name="privatecircle" v-model="privatecircle" id="privatecircle" />
+      </div>
+      <div v-else>(You do not have any Private Circles, so options are hidden)</div>
+    </article>
+    <button type="submit">
+      Create a freet
+    </button>
+    <section class="alerts">
+      <article v-for="(status, alert, index) in alerts" :key="index" :class="status">
+        <p>{{ alert }}</p>
+      </article>
+    </section>
+  </form>
+</template>
 
 <script>
-import BlockForm from '@/components/common/BlockForm.vue';
 
 export default {
-  name: 'CreateFreetForm',
-  mixins: [BlockForm],
+  name: 'BlockForm',
+  async mounted() {
+    await this.$store.commit('refreshPrivateCircles');
+  },
   data() {
     return {
-      url: '/api/freets',
-      method: 'POST',
-      hasBody: true,
-      fields: [
-        { id: 'content', label: 'Content', value: '' },
-        { id: 'private_circle', label: 'Private Circle (Optional)', value: '' }
-      ],
-      title: 'Create a freet',
-      refreshFreets: true,
-      callback: () => {
+      content: '',
+      privatecircle: '',
+      alerts: {},
+    };
+  },
+  methods: {
+    async submit() {
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'same-origin'
+      };
+      const fields = {
+        content: this.content,
+        private_circle: this.privatecircle
+      }
+      options.body = JSON.stringify(fields);
+
+      try {
+        const r = await fetch('/api/freets', options);
+        if (!r.ok) {
+          const res = await r.json();
+          throw new Error(res.error);
+        }
+
+        this.$store.commit('refreshFreets');
+
+        this.content = '';
+        this.privatecircle = '';
+
         const message = 'Successfully created a freet!';
         this.$set(this.alerts, message, 'success');
         setTimeout(() => this.$delete(this.alerts, message), 3000);
+
+      } catch (e) {
+        this.$set(this.alerts, e, 'error');
+        setTimeout(() => this.$delete(this.alerts, e), 3000);
       }
-    };
+    }
   }
 };
 </script>
+
+<style scoped>
+form {
+  border: 1px solid #111;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 14px;
+  position: relative;
+}
+
+article>div {
+  display: flex;
+  flex-direction: column;
+}
+
+form>article p {
+  margin: 0;
+}
+
+form h3,
+form>* {
+  margin: 0.3em 0;
+}
+
+form h3 {
+  margin-top: 0;
+}
+
+textarea {
+  font-family: inherit;
+  font-size: inherit;
+}
+</style>
